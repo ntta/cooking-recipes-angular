ff1f96406c6d0f2d555dd77d24102fc8b333908d
ntta <aaron.ntta@gmail.com>
2020-08-10 15:56:38 +1000

    :sparkles:  Remove old href, use route link instead

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,18 +1,8 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
-  @Output() featureSelected = new EventEmitter<string>();
-
-  constructor() {}
-
-  ngOnInit(): void {}
-
-  onSelect(feature: string) {
-    this.featureSelected.emit(feature);
-  }
-}
+export class HeaderComponent implements OnInit {}
